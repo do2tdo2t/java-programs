d04263f1ea9089b28e8a9e2c58919977e7c741e5
HyojungPark <hj__park@naver.com>
2018-08-17 11:08:51 +0900

    finish index -> recommendation accomodation
    index.jsp 페이지  내에서 추천 창 추가 완료

--- checkinTest/src/main/java/com/checkin/webapp/accomodation/AccomodationController.java
+++ checkinTest/src/main/java/com/checkin/webapp/accomodation/AccomodationController.java
@@ -1,10 +1,15 @@
 package com.checkin.webapp.accomodation;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.checkin.webapp.accomodation.command.RecommendCommand;
 import com.checkin.webapp.accomodation.model.AccomodationVO;
 
 @Controller
@@ -43,4 +48,14 @@ public class AccomodationController {
 	// 매핑 경로 : /webmaster/showAccoList
 	public ModelAndView showListWebmaster(HttpServletRequest request, AccomodationVO vo) {}
 	*/
+	
+	//main index page ajax 처리
+	// 매핑 경로 : /main/recommendAjax
+	@RequestMapping("/main/recommend")
+	@ResponseBody
+	public List<AccomodationVO> recommendAjax(HttpServletRequest request, AccomodationVO vo) {
+		System.out.println("AccomodationController..."+vo.toString());
+		return new RecommendCommand().execute2(request,vo);
+	}
+	
 }
--- checkinTest/src/main/java/com/checkin/webapp/webmaster/command/InsertNoticeOkCommand.java
+++ checkinTest/src/main/java/com/checkin/webapp/webmaster/command/InsertNoticeOkCommand.java
@@ -6,14 +6,13 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.checkin.webapp.Constants;
 import com.checkin.webapp.webmaster.model.NoticeDAOInterface;
 import com.checkin.webapp.webmaster.model.NoticeVO;
 
 public class InsertNoticeOkCommand implements WebmasterCommandInterface {
-	
-	@Autowired
-	public SqlSession sqlSession;
-	
+
+
 	@Override
 	public ModelAndView execute(HttpServletRequest request, NoticeVO vo) {
 		System.out.println("InsertNoticeOkCommand. execute()");
@@ -24,12 +23,17 @@ public class InsertNoticeOkCommand implements WebmasterCommandInterface {
 		test.setNotitle("연휴 관련 공지 입니다.");
 		test.setTarget(0);
 		test.setWid("webm");
+
 		
-		NoticeDAOInterface mapper = sqlSession.getMapper(com.checkin.webapp.webmaster.model.NoticeDAOInterface.class);
-		int result = mapper.insertNotice(test);
-		System.out.println("InsertNoticeOkCommand.. call insertNotice().."+result);
-		
-		return null;
+		if (Constants.sqlSession != null) {
+			System.out.println("sqlSession is not null....");
+			NoticeDAOInterface mapper = Constants.sqlSession.getMapper(NoticeDAOInterface.class);
+			int result = mapper.insertNotice(test);
+			System.out.println("InsertNoticeOkCommand.. call insertNotice().." + result);
+		} else {
+			System.out.println("sqlSession is null....");
+		}
+		return mav;
 	}
 
 }
